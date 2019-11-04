0f8c9416797a8f691bf89ffb25b15521cb161d3d
David Delgado <dadch13.14@gmail.com>
2019-11-03 20:55:00 -0400

    Video 'Linking events' of the module 'Creation and design of components'.

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -4,6 +4,7 @@ import "./styles/BadgeNew.css";
 import header from "../images/badge-header.svg";
 import Navbar from "../components/Navbar";
 import Badge from "../components/Badge";
+import BadgeForm from "../components/BadgeForm";
 
 class BadgeNew extends React.Component {
 	render() {
@@ -16,7 +17,7 @@ class BadgeNew extends React.Component {
 
 				<div className="container">
 					<div className="row">
-						<div className="col">
+						<div className="col-6">
 							<Badge
 								firstName="David"
 								lastname="Delgado"
@@ -24,6 +25,10 @@ class BadgeNew extends React.Component {
 								jobTitle="Ingeniero en Informática"
 								avatarUrl="https://www.gravatar.com/avatar/avatar?d=identicon"
 							/>
+						</div>
+
+						<div className="col-6">
+							<BadgeForm />
 						</div>
 					</div>
 				</div>
