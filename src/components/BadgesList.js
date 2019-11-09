c8abac56e4dfd170f0661822bec44b9a1a8e1f3d
David Delgado <dadch13.14@gmail.com>
2019-11-09 12:51:26 -0400

    Video 'Updating data (PUT)' of the module 'Calls to an API'.

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -5,6 +5,28 @@ import twitterLogo from "../images/twitter-brands.svg";
 import { Link } from "react-router-dom";
 import Gravatar from "./Gravatar";
 
+class BadgesListItem extends React.Component {
+  render() {
+    return (
+      <div className="badge-item">
+        <div className="badge-item__avatar">
+          <Gravatar email={this.props.badge.email} />
+        </div>
+        <div className="badge-item__info">
+          <h5 className="font-weight-bold">
+            {this.props.badge.firstName} {this.props.badge.lastName}
+          </h5>
+          <p className="badge-item__twitter">
+            <img className="img-fluid" src={twitterLogo} alt="Twitter" />@
+            {this.props.badge.twitter}
+          </p>
+          <p>{this.props.badge.jobTitle}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
 class BadgesList extends React.Component {
   render() {
     if (this.props.badges.length === 0) {
@@ -23,25 +45,9 @@ class BadgesList extends React.Component {
         {this.props.badges.map(badge => {
           return (
             <li key={badge.id}>
-              <div className="badge-item">
-                <div className="badge-item__avatar">
-                  <Gravatar email={badge.email} />
-                </div>
-                <div className="badge-item__info">
-                  <h5 className="font-weight-bold">
-                    {badge.firstName} {badge.lastName}
-                  </h5>
-                  <p className="badge-item__twitter">
-                    <img
-                      className="img-fluid"
-                      src={twitterLogo}
-                      alt="Twitter"
-                    />
-                    @{badge.twitter}
-                  </p>
-                  <p>{badge.jobTitle}</p>
-                </div>
-              </div>
+              <Link className="text-reset text-decoration-none" to={`badges/${badge.id}/edit`}>
+                <BadgesListItem badge={badge} />
+              </Link>
             </li>
           );
         })}
--- src/pages/BadgeEdit.js
+++ src/pages/BadgeEdit.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-import "./styles/BadgeNew.css";
+import "./styles/BadgeEdit.css";
 import header from "../images/platzi-conf.svg";
 import Badge from "../components/Badge";
 import BadgeForm from "../components/BadgeForm";
 import api from "../api";
 import PageLoading from "../components/PageLoading";
 
-class BadgeNew extends React.Component {
+class BadgeEdit extends React.Component {
   state = {
     loading: false,
     error: null,
@@ -20,16 +20,22 @@ class BadgeNew extends React.Component {
     }
   };
 
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData = async e => {
+    this.setState({ loading: true, error: null });
+
+    try {
+      const data = await api.badges.read(this.props.match.params.badgeId);
+      this.setState({ loading: false, form: data });
+    } catch (error) {
+      this.setState({ loading: false, error });
+    }
+  };
+
   handleChange = e => {
-    // Solución 1 para evitar la sobreescritura de form
-    // const nextForm = this.state.form;
-    // nextForm[e.target.name] = e.target.value;
-
-    // this.setState({
-    // 	form: nextForm
-    // });
-
-    // Solución 2 para evitar la sobreescritura de form
     this.setState({
       form: {
         ...this.state.form,
@@ -47,7 +53,7 @@ class BadgeNew extends React.Component {
     });
 
     try {
-      await api.badges.create(this.state.form);
+      await api.badges.update(this.props.match.params.badgeId, this.state.form);
       this.setState({ loading: false });
 
       this.props.history.push("/badges");
@@ -63,9 +69,9 @@ class BadgeNew extends React.Component {
 
     return (
       <React.Fragment>
-        <div className="BadgeNew__hero">
+        <div className="BadgeEdit__hero">
           <img
-            className="BadgeNew__hero-image img-fluid"
+            className="BadgeEdit__hero-image img-fluid"
             src={header}
             alt="Logo"
           />
@@ -85,7 +91,7 @@ class BadgeNew extends React.Component {
             </div>
 
             <div className="col-6">
-              <h1>New Attendant</h1>
+              <h1>Edit Attendant</h1>
               <BadgeForm
                 onChange={this.handleChange}
                 onSubmit={this.handleSubmit}
@@ -100,4 +106,4 @@ class BadgeNew extends React.Component {
   }
 }
 
-export default BadgeNew;
+export default BadgeEdit;
